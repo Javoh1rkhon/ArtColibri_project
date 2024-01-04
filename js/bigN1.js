7d8ebfe4d862c4a98db7837a8fabc955d7be46b3
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2024-01-04 15:12:04 +0500

    yarimi bajarildi

---
+++ js/bigN1.js
@@ -0,0 +1,5 @@
+let see_all1 = document.querySelector('#see_all1')
+
+see_all1.addEventListener('click', () => {
+    location.href = "/html/katalogN2.html"
+})
---
+++ js/katalogN2.js
@@ -0,0 +1,4 @@
+let glavniy = document.querySelector('.glavniy')
+glavniy.addEventListener('click', () => {
+    location.href = "/html/bigN1.html"
+})
